b30aaf79f4620cb85b182a5349ac3c59c387ea55
Fernando Dourado <fernando.dourado@harness.io>
2026-02-11 17:42:34 -0300

    Update main.tf

--- terraform/secret-manager-issue/main.tf
+++ terraform/secret-manager-issue/main.tf
@@ -8,16 +8,17 @@ terraform {
 }
 
 provider "harness" {
-  endpoint         = "https://app.harness.io/gateway"
+  endpoint         = "https://qa.harness.io"
   account_id       = var.harness_account_id
   platform_api_key = var.harness_api_key
 }
 
 resource "harness_platform_service" "service_remote_template" {
   identifier  = "service_remote_template"
-  name        = "service-remote-template.yaml"
-  org_id      = "var.org_id"
-  project_id  = "var.project_id"
+  name        = "service-remote-template"
+  org_id      = var.org_id
+  project_id  = var.project_id
+  import_from_git = "true"
   git_details {
     branch         = "main"
     commit_message = "tf apply service creation"
