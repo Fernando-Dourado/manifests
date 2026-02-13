b468cd51b436b7b13be5406a05706b000c64be74
Fernando Dourado <fernando.dourado@harness.io>
2026-02-13 10:13:22 -0300

    Update main.tf

--- terraform/secret-manager-issue/main.tf
+++ terraform/secret-manager-issue/main.tf
@@ -27,19 +27,3 @@ resource "harness_platform_connector_github" "ghFD_Manifests" {
     }
   }
 }
-
-resource "harness_platform_service" "service_remote_template" {
-  identifier  = "service_remote_template"
-  name        = "service-remote-template"
-  org_id      = var.org_id
-  project_id  = var.project_id
-  import_from_git = "true"
-  git_details {
-    branch         = "main"
-    commit_message = "tf apply service creation"
-    file_path      = ".harness/service-remote-template.yaml"
-    connector_ref  = "ghFD_Manifests_123"
-    store_type     = "REMOTE"
-    repo_name      = "manifests"
-  }
-}
