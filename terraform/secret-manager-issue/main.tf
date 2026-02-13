0f7c9e6bd5db8bf65434c923b1e2f270cf67ed3f
Fernando Dourado <fernando.dourado@harness.io>
2026-02-13 16:19:27 -0300

    Update main.tf

--- terraform/secret-manager-issue/main.tf
+++ terraform/secret-manager-issue/main.tf
@@ -12,20 +12,8 @@ terraform {
   }
 }
 
-variable "aws_access_key" {
-  type      = string
-  sensitive = true
-}
- 
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
-}
-
 provider "aws" {
   region = "us-east-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 provider "harness" {
