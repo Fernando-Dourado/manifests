cc5127a18e740de68f9460ca9f39d3f4fc38afd0
Fernando Dourado <fernando.dourado@harness.io>
2026-02-19 16:07:50 -0300

    Update main.tf

--- terraform/secret-manager-issue/main.tf
+++ terraform/secret-manager-issue/main.tf
@@ -57,3 +57,126 @@ resource "harness_platform_service" "service_remote_template" {
   }
   depends_on = [harness_platform_connector_github.ghFD_Manifests]
 }
+
+resource "harness_platform_pipeline" "tf_sm_issue" {
+  identifier 	= "Terraform_Secret_Manager_Issue"
+  org_id      	= var.org_id
+  project_id  	= var.project_id
+  name       	= "Terraform Secret Manager Issue"
+  yaml = <<-EOT
+	pipeline:
+	  name: Terraform Secret Manager Issue
+	  identifier: Terraform_Secret_Manager_Issue
+	  projectIdentifier: FernandoD
+	  orgIdentifier: default
+	  tags: {}
+	  stages:
+	    - stage:
+	        name: TF
+	        identifier: TF
+	        description: ""
+	        type: CI
+	        spec:
+	          cloneCodebase: false
+	          caching:
+	            enabled: true
+	            override: true
+	            paths: []
+	          buildIntelligence:
+	            enabled: true
+	          execution:
+	            steps:
+	              - step:
+	                  type: GitClone
+	                  name: Clone
+	                  identifier: Clone
+	                  spec:
+	                    connectorRef: ghFD_Manifests_123
+	                    build:
+	                      type: branch
+	                      spec:
+	                        branch: main
+	              - step:
+	                  type: Run
+	                  name: Init
+	                  identifier: Init
+	                  spec:
+	                    connectorRef: Public_Docker_Hub
+	                    image: hashicorp/terraform:1.11.4
+	                    shell: Sh
+	                    command: |-
+	                      echo "tfenv use 1.11.4 <DISABLED>"
+
+	                      cd /harness/manifests/terraform/secret-manager-issue
+	                      echo "Terraform Execution starts"
+	                      ls -l
+
+	                      terraform init -reconfigure \
+	                        -backend-config="access_key=<+pipeline.variables.AWS_ACCESS_KEY>" \
+	                        -backend-config="secret_key=<+pipeline.variables.AWS_SECRET_KEY>"
+	              - step:
+	                  type: Run
+	                  name: Plan
+	                  identifier: Plan
+	                  spec:
+	                    connectorRef: Public_Docker_Hub
+	                    image: hashicorp/terraform:1.11.4
+	                    shell: Sh
+	                    command: |-
+	                      cd /harness/manifests/terraform/secret-manager-issue
+
+	                      terraform plan \
+	                        -var=harness_account_id=zEaak-FLS425IEO7OLzMUg \
+	                        -var=harness_api_key=<+pipeline.variables.apiKey> \
+	                        -var-file=./terraform.tfvars
+	                  when:
+	                    stageStatus: Success
+	                    condition: "false"
+	              - step:
+	                  type: Run
+	                  name: Apply
+	                  identifier: Apply
+	                  spec:
+	                    connectorRef: Public_Docker_Hub
+	                    image: hashicorp/terraform:1.11.4
+	                    shell: Sh
+	                    command: |-
+	                      cd /harness/manifests/terraform/secret-manager-issue
+
+	                      #export TF_LOG=DEBUG
+
+	                      terraform apply -auto-approve \
+	                        -var=harness_account_id=zEaak-FLS425IEO7OLzMUg \
+	                        -var=harness_api_key=<+pipeline.variables.apiKey> \
+	                        -var-file=./terraform.tfvars
+	          infrastructure:
+	            type: KubernetesDirect
+	            spec:
+	              connectorRef: fdcluster
+	              namespace: harness-delegate-ng
+	              automountServiceAccountToken: true
+	              nodeSelector: {}
+	              os: Linux
+	        delegateSelectors:
+	          - cv-cg-test-delegate
+	  variables:
+	    - name: apiKey
+	      type: Secret
+	      description: ""
+	      required: false
+	      value: FernandoD_TF_Token
+	    - name: AWS_SECRET_KEY
+	      type: Secret
+	      description: ""
+	      required: false
+	      value: CD_Play_--_Admin_Secret_Key
+	    - name: AWS_ACCESS_KEY
+	      type: Secret
+	      description: ""
+	      required: false
+	      value: CD_Play_--_Admin_Access_Key
+	  delegateSelectors:
+	    - cv-cg-test-delegate
+  EOT
+  depends_on = [harness_platform_connector_github.ghFD_Manifests]
+}
